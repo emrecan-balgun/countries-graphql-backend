3f19773307b69781a91f925c5d99dfe3c45969aa
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-24 15:09:12 +0300

    update data and index.js

--- data.js
+++ data.js
@@ -1,50 +1,34 @@
-const authors = [{
-    id: "1",
-    name: "Albert",
-    surname: "Camus",
-    age: "41",
-    },
-    {
-        id: "2",
-        name: "Robert",
-        surname: "Cervantes",
-        age: "33",
-        books: []
-    }
+const countries = [ 
+    {id: 1, name: 'France', code: 'FR'}, //
+    {id: 2, name: 'Germany', code: 'DE'}, //
+    {id: 3, name: 'Italy', code: 'IT'}, //
+    {id: 4, name: 'Poland', code: 'PL'}, //
+    {id: 5, name: 'Portugal', code: 'PT'}, //
+    {id: 6, name: 'Turkey', code: 'TR'}, //
+  ]
+
+const languages = [
+    {id: 1, name: 'French'},
+    {id: 2, name: 'German'},
+    {id: 3, name: 'Italian'},
+    {id: 4, name: 'Polish'},
+    {id: 5, name: 'Portuguese'},
+    {id: 6, name: 'Turkish'},
 ]
 
-const books = [
-    {
-        id: "1",
-        title: "Yabancı",
-        author_id: "1",
-        score: 3.4,
-        isPublished: true,
-    },
-    {
-        id: "2",
-        title: "Hırsız",
-        author_id: "2",
-        score: 7.1,
-        isPublished: false,
-    },
-    {
-        id: "3",
-        title: "Düzenbaz",
-        author_id: "1",
-        score: 8.4,
-        isPublished: true,
-    },
-    {
-        id: "4",
-        title: "Denemeler",
-        author_id: "2",
-        score: 5.9,
-        isPublished: false,
-    },
+const continents = [
+    {id: 1, name: "Africa", code: "AF"},
+    {id: 2, name: "Antarctica", code: "AN"},
+    {id: 3, name: "Asia", code: "AS"},
+    {id: 4, name: "Europe", code: "EU"},
+    {id: 5, name: "North America", code: "NA"}, 
+    {id: 6, name: "Oceania", code: "OC"},
+    {id: 7, name: "South America", code: "SA"},
 ]
+
 
 module.exports = {
-    authors,
-    books
+    countries,
+    languages,
+    continents,
 }
--- index.js
+++ index.js
@@ -1,29 +1,99 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
+const { languages, continents, countries } = require('./data');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
+    # type Author {
+    #     id: ID!
+    #     name: String!
+    #     surname: String
+    #     age: Int
+    #     books(filter: String): [Book!] # if the book exists it cannot be null
+    # }
 
-  type Query {
-    books: [Book]
-  }
+    type Country {
+        id: ID!
+        name: String!
+        code: String!
+    }
+
+    type Language {
+        id: ID!
+        name: String!
+    }
+
+    type Continent {
+        id: ID!
+        name: String!
+        code: String!
+    }
+
+    # type Book {
+    #     id: ID!
+    #     title: String!
+    #     author: Author
+    #     author_id: ID!
+    #     score: Float
+    #     isPublished: Boolean
+    # }
+
+    type Query {
+        # books: [Book!] # an array can be empty, but the object inside cannot be null
+        # book(id: ID!): Book!
+
+        countries: [Country!]
+        
+
+        languages: [Language!]
+
+
+        continents: [Continent!]
+
+        # authors: [Author!]
+        # author(id: ID!): Author!
+    }
 `;
 
 const resolvers = {
     Query: {
-    //   books: () => books,
-    books: () => [{ title: "Title", author: "Albert Camus"}],
-    },
-  };
+        // books: () => books,
 
-  const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [
+        // book: (parent, args) => books.find(book => book.id === args.id),
+
+        countries: () => countries,
+        // country: (parent, args) => countries.find(country => country.code === args.code),
+
+        languages: () => languages,
+
+        continents: () => continents,
+
+        // authors: () => authors,
+
+        // author: (parent, args) => authors.find(author => author.id === args.id),  
+    },
+
+    // Book: {
+    //     author: (parent) => authors.find((author) => author.id ===  parent.author_id),
+    // },
+
+    // Author: {
+    //     books: (parent, args) => {
+    //     let filtered = books.filter((book) => book.author_id === parent.id)
+
+    //     if(args.filter) {
+    //         filtered = filtered.filter((book) => book.title.toLowerCase().startsWith(args.filter))
+    //     }
+        
+    //     return filtered
+    // }
+    // }
+};
+
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground({
       // options
     })
@@ -31,5 +101,5 @@ const resolvers = {
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+    console.log(`Apollo server is up at ${url}`);
 });
